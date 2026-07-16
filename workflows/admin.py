45f85821f3259a54187bc5d2245c16866b96b085
k_stein <karlasteinbrink@gmail.com>
2026-07-16 11:37:03 +0200

    Register workflows models in admin panel

--- workflows/admin.py
+++ workflows/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import N8nConnection, GeneratedWorkflow
 
 # Register your models here.
+class N8nConnectionAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'is_verified',
+    )
+
+class GeneratedWorkflowAdmin(admin.ModelAdmin):
+    list_display = (
+        'workflow_id',
+        'status',
+        'created_at',
+    )
+    ordering = ('-created_at',)
+
+
+admin.site.register(N8nConnection, N8nConnectionAdmin)
+admin.site.register(GeneratedWorkflow, GeneratedWorkflowAdmin)
